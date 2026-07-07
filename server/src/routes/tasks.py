ce50405cfeac7326aafda177b496c03b6e688805
biogerm <biogerm@gmail.com>
2026-07-07 15:18:24 +0200

    feat(integration): integrate M8 carousel and M9 focus view

--- server/src/routes/tasks.py
+++ server/src/routes/tasks.py
@@ -8,16 +8,29 @@ tasks_bp = Blueprint("tasks", __name__)
 def get_tasks():
     try:
         from src.app import merger
+
         data = merger.get_data()
     except Exception:
         try:
             from app import merger
+
             data = merger.get_data()
         except Exception:
             data = current_app.merger.get_data()
 
     from src.utils.i18n import get_locale
+
     data["locale"] = get_locale()
+
+    try:
+        from src.utils.config import load_config
+
+        config = load_config()
+        data["carousel"] = config.get(
+            "carousel", {"intervalSeconds": 300, "screens": [0, 1]}
+        )
+    except Exception:
+        data["carousel"] = {"intervalSeconds": 300, "screens": [0, 1]}
 
     response = jsonify(data)
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
